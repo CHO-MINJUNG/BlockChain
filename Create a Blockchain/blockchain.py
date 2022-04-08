4447be213dad673ba54bdb9944d51b4055e2764f
MINJUNG-CHO <61917990+CHO-MINJUNG@users.noreply.github.com>
2022-04-08 18:06:48 +0900

    작업증명 함수 추가

--- Create a Blockchain/blockchain.py
+++ Create a Blockchain/blockchain.py
@@ -15,5 +15,29 @@ class Blockchains:
         self.chain = []
         # prev_hash가 0인 이유는 해당 block이 genesis이기 때문에
         self.create_block(proof=1, previous_hash = '0')
-
+    
+    # 작업 증명, 블록 채굴 직 후 사용할 함
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof=True
+            else:
+                new_proof += 1 
+        return new_proof
 # 2. block chain state 표시 함수(postman) + new block mining 함수
+
+
